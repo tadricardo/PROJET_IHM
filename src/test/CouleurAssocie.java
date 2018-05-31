93c3865ea310d6cc0c614f4e472bc476d1a4d2b6
alexandre-duhamel <alexandreduhamel59@gmail.com>
2018-05-31 02:38:03 +0200

    l'algo renvoi maintenant des nuance de toutes les couleurs et non pas que des nuances de bleu

--- src/test/CouleurAssocie.java
+++ src/test/CouleurAssocie.java
@@ -1,5 +1,11 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 import javafx.scene.paint.Color;
 
 public class CouleurAssocie {
@@ -8,32 +14,37 @@ public class CouleurAssocie {
 	private double vert=0;
 	private double rouge=0;
 	private double reste;
-	
+
 	public Color couleur;
 
 	CouleurAssocie(int a){
+
 		reste=a/255.0;
-			
-		bleu=reste/0.11; //on rempli bleu	
-		reste=0;
-		if (bleu>=1.0) { //si bleu est trop plein ,on rempli vert	
-			reste+=(bleu-1.0)*0.11;
-			bleu=1.0;
-			
-			vert=reste/0.59;
-			reste=0;
-			if (vert>=1.0) { //si vert est trop plein ,on rempli rouge
-				reste+=(vert-1.0)*0.59;
-				vert=1.0;
+		ThreadLocalRandom random = ThreadLocalRandom.current();
+
+		List<Integer> lstIndexes = new ArrayList<Integer>();
+		lstIndexes.add(1);
+		lstIndexes.add(2);
+		lstIndexes.add(3);
+
+		Collections.shuffle(lstIndexes);
+		for(Integer index : lstIndexes) {
+			switch(index) {
+			case 1:
+				bleu=random.nextDouble(reste-1E-15, Math.min(1.0, reste/0.11));
+				reste-=bleu*0.11;
+				break;
+			case 2: 	
+				rouge=random.nextDouble(reste-1E-15, Math.min(1.0, reste/0.3));
+				reste-=rouge*0.3;
+				break;
+			case 3: 
+				vert=random.nextDouble(reste-1E-15, Math.min(1.0, reste/0.59));
+				reste-=vert*0.59; 
+				break;
 			}
-			
-			rouge=reste/0.3;
-			reste=0;
-			if (rouge>=1.0) {
-				reste+=(rouge-1.0)*0.3;
-				rouge=1.0;
-			}		
-		}		
-		couleur=new Color(rouge, vert, bleu, 1);		
+		}
+
+		couleur=new Color(rouge, vert, bleu, 1);	
 	}
 }
